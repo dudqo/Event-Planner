f6a06c51d4e3b004b17add9f8f7805ca556f8fe8
Young Suk <sukyb0815@hotmail.com>
2023-03-30 17:49:36 -0400

    google maps on home screen

--- app/src/main/java/com/example/eventplanner/screens/HomeScreen.kt
+++ app/src/main/java/com/example/eventplanner/screens/HomeScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberScaffoldState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.google.maps.android.compose.GoogleMap
@@ -16,13 +17,17 @@ fun HomeScreen(
     viewModel: MapViewModel = viewModel()
 ) {
     val scaffoldState = rememberScaffoldState()
+    val uiSettings = remember {
+        MapUiSettings(zoomControlsEnabled = false)
+    }
     Scaffold(
         scaffoldState = scaffoldState
 
     ) {
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
-            properties = viewModel.state.properties
+            properties = viewModel.state.properties,
+            uiSettings = uiSettings
         )
     }
 }
