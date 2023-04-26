ad39a667755a83338c79f081c4dd774a854d2677
Young Suk <sukyb0815@hotmail.com>
2023-04-26 10:40:53 -0400

    name fix

--- app/src/main/java/com/dudqo/eventplanner/screens/sign_in/SignInViewModel.kt
+++ app/src/main/java/com/dudqo/eventplanner/screens/sign_in/SignInViewModel.kt
@@ -5,7 +5,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 
-class SignInViewModiel: ViewModel() {
+class SignInViewModel: ViewModel() {
 
     private val _state = MutableStateFlow(SignInState())
     val state = _state.asStateFlow()
